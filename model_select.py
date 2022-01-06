002d5136a4c343658bdecde2764bf39e1c430c47
Tyler <tymarbach@gmail.com>
2022-01-05 20:13:09 -0800

    added modelselector func and option for kmeans
    new function will allow for model arg to be used and accounts
    for the input differences per model.

--- model_select.py
+++ model_select.py
@@ -25,8 +25,8 @@ def arguments():
             "-w",
             "--window_size",
             help="Directs the output to a name of your choice",
-            default=False)
-
+            default=False, type=int)
+            # check for int, throw error if not int
     parser.add_argument(
             "-o",
             "--output_file",
@@ -35,33 +35,54 @@ def arguments():
     return parser.parse_args()
 
 
-def output_data(original_data, clean_csv_df, output_location):
+def output_data(reportdf, modwinpar_list, output_filename):
+
         """
         Input:
         score_tuple -- various accuracy scores in tuple form
-        output_file_name -- title for output filename
+        modwinpar_list -- list of modelname, windowsize, parameters
+        output_filename -- title for output filename
+
         Output:
         summary_stats -- print out summary stats after running
         recorded stats -- record stats in output file
         """
-        if os.path.exists(filename):
-        append_write = 'a' # append if already exists
+        df = reportdf.append(modwinpar_list, ignore_index=True, header=False)
+        df.append(pd.Series(), ignore_index=True)
+        if os.path.exists(output_filename):
+                df.to_csv(output_filename, mode='a')# append if already exists
+        elif output_filename == False:
+                df.to_csv('summary_scores_'+str(modwinpar_list[0]), index=False)
         else:
-        append_write = 'w'
-    filename = os.path.basename(original_data)
-    if output_location == False:
-        clean_csv_df.to_csv('summary_scores_'+filename, index=False)
-    else:
-        clean_csv_df.to_csv(output_location, index=False)
+                df.to_csv(output_filename, index=False)
+
+
+def model_flow(model, Xdata, X_train, X_test, y_train, y_test, classes):
+        model = str(model)
+        if model == 'svm':
+                svmreport = svm(X_train, X_test, y_train, y_test)
+                return svmreport
+        elif model == 'rf':
+                rfreport = forester(X_train, X_test, y_train, y_test)
+                return rfreport
+        elif model == 'kmeans':
+                kmreport = kmeans(Xdata,classes)
+                return kmreport
+        
 
 
 def main():
     args = arguments()
     df = pd.read_csv("~/CNNworkspace/raterdata/dec21_cleanPennf1.csv")
-    windows = pull_window(df, int(args.window_size))
+    windows, classes = pull_window(df, int(args.window_size))
     Xdata, ydata = construct_xy(windows)
     X_train, X_test, y_train, y_test = reduce_dimesions(Xdata,ydata)
-    print(forester(X_train, X_test, y_train, y_test))
+    report = model_flow(args.model, X_train, X_test, y_train, y_test, classes)
+    #report = forester(X_train, X_test, y_train, y_test, classes)
+    reportdf = pd.DataFrame(report).transpose()
+    #reportdf.to_csv('svm_stats.csv')
+    output_data(reportdf,BLANK,args.output_file)
+    print()
     #output_data(args.csv_file, clean_data, args.output)
     # return output_data which will be a csv file of the cleaned
     # and reorganized data, other scripts will work with it from there.
