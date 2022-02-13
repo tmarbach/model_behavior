776f743dc38ea999928df10337b1b1609df8fad9
Tyler <tymarbach@gmail.com>
2022-02-13 14:14:16 -0800

    Added several func that alter the input
    added transform_xy, reduce_dim_strat, rando_maker, and class_divider
    they transform the windows, reduce the dimensions of the windows while
    taking a stratified sample from each class, create equal class sizes of
    windows for even training and testing, and finally, apply leaping windows
    to some of the classes and a sliding window to others, RESPECTIVELY.

--- window_maker.py
+++ window_maker.py
@@ -1,12 +1,22 @@
 import pandas as pd
 import numpy as np
+import random
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import StratifiedShuffleSplit
 
 
+def class_divider(df, window_size):
+    majoritydf = df[(df['behavior']=='s') | (df['behavior']=='l')]
+    minoritydf = df[(df['behavior']!='s') & (df['behavior']!='l')]
+    maj_windows , maj_classes = pull_window(majoritydf, window_size)
+    min_windows , min_classes = pull_window(minoritydf, window_size)
+    all_windows = maj_windows+min_windows
+    all_classes = maj_classes+min_classes
+    return all_windows, all_classes
 
+#df[[ behavior,acc_x,acc_y,acc_z]].describe()
 
-def pull_window(df, window_size):
+def pull_window(df, window_size, class_list):
     """
     Input: 
     df -- dataframe of cleaned input data, likely from a csv
@@ -25,12 +35,66 @@ def pull_window(df, window_size):
         if len(set(window.behavior)) != 1:
             continue
         if len(set(np.ediff1d(window.input_index))) != 1:
-             continue
+            continue
+        if window.iloc[0]['behavior'] not in class_list:
+            continue
         windows.append(window)
         classes.append(window.iloc[0]['behavior'])
     allclasses = set(classes)
     print("Windows pulled")
     return windows, list(allclasses)
+
+
+def rando_maker(df, window_size, class_list):
+    allclasswins = []
+    classcodes = []
+    for classes in class_list:
+        classdf, clist = pull_window(df, window_size, classes)
+        rands = random.sample(classdf, 500)
+        classcodes += clist
+        allclasswins += rands
+    return allclasswins, classcodes
+
+
+# add a classifier list for classes to pay attention to 
+# def pull_window(df, window_size):
+#     """
+#     Input: 
+#     df -- dataframe of cleaned input data, likely from a csv
+#     window_size -- number of rows of data to convert to 1 row for AcceleRater (25 = 1sec)
+#     Output:
+#     windows -- list of lists of accel data (EX:[x,y,z,...,x,y,z,class_label])
+#     allclasses -- list of the behavior classes that are present in the windows
+#     """
+#     classes = []
+#     windows = []
+#     number_of_rows_minus_window = df.shape[0] - window_size + 1
+
+#     if window_size > df.shape[0]:
+#         raise ValueError('Window larger than data given')
+#     if df.iloc[0]['behavior'] == 's' or df.iloc[0]['behavior'] == 'l':
+#         for i in range(0, number_of_rows_minus_window, window_size):
+#             window = df[i:i+window_size]
+#             if len(set(window.behavior)) != 1:
+#                 continue
+#             if len(set(np.ediff1d(window.input_index))) != 1:
+#                 continue
+#             windows.append(window)
+#             classes.append(window.iloc[0]['behavior'])
+#             #
+#     else:
+#         for i in range(0, number_of_rows_minus_window, 1):
+#             window = df[i:i+window_size]
+#             if len(set(window.behavior)) != 1:
+#                 continue
+#             if len(set(np.ediff1d(window.input_index))) != 1:
+#                 continue
+#             windows.append(window)
+#             classes.append(window.iloc[0]['behavior'])
+#             #
+#     allclasses = set(classes)
+#     print("Windows pulled")
+#     return windows, list(allclasses)
 
 
 def construct_xy(windows, classdict):
@@ -41,21 +105,43 @@ def construct_xy(windows, classdict):
         Xdata -- arrays of xyz data of each window stacked together
         ydata -- integer class labels for each window
     """
-    positions = ['acc_x', 'acc_y', 'acc_z']
+    positions = ['accX', 'accY', 'accZ']
     Xdata, ydata = [], []
     for window in windows:
         Xdata.append(window[positions].to_numpy())
         ydata.append(classdict[window['behavior'].iloc[0]])
         
     return np.stack(Xdata), np.asarray(ydata)
+    
+
+def transform_xy(windows, classdict):
+    """
+    Input:
+        windows -- list of dataframes of all one class 
+    Output:
+        Xdata -- arrays of xyz data of each window stacked together
+        ydata -- integer class labels for each window
+    """
+    #positions = ['accX', 'accY', 'accZ']
+    Xdata, ydata = [], []
+    for window in windows:
+        Xdata.append(np.array([window.accX.mean(), window.accY.mean()]))
+        ydata.append(classdict[window['behavior'].iloc[0]])
+        
+    return np.stack(Xdata), np.asarray(ydata)
 
 
+
+
+#OLD method
 def reduce_dimensions(Xdata, ydata):
     nsamples, nx, ny = Xdata.shape
     Xdata2d = Xdata.reshape((nsamples,nx*ny))
     X_train, X_test, y_train, y_test = train_test_split(
         Xdata2d, ydata, test_size=0.2, random_state=42)
     return X_train, X_test, y_train, y_test
+#
+
 
 
 def reduce_dim_strat(Xdata,ydata):
