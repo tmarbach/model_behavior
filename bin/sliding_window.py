2e943930cb40e5ef2e656027c0a14f871472c0c8
Tyler <tymarbach@gmail.com>
2022-02-15 11:32:59 -0800

    made new dir for code, sliding_window func
    Reorganizing code for better implementation. New func
    is an adjustment from the pull_windows func, will be
    used to create better starting data.

---
+++ bin/sliding_window.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import numpy as np
+
+
+
+def slide_window(df, window_size, slide: int = 1):
+    """
+    Input: 
+    df -- dataframe of input data, likely from a csv
+    window_size -- number of rows of data to convert to 1 row for AcceleRater (25 = 1sec)
+    Output:
+    windows -- list of dataframes of accel data
+    allclasses -- list of the behavior classes that are present in the windows
+    """
+    classes = []
+    windows = []
+    number_of_rows_minus_window = df.shape[0] - window_size + 1
+    if window_size > df.shape[0]:
+        raise ValueError('Window larger than data given')
+    for i in range(0, number_of_rows_minus_window, slide):
+        window = df[i:i+window_size]
+        windows.append(window)
+        classes.append(list(window.Behavior.unique().sum()))
+    allclasses = set(classes)
+    print("Windows pulled")
+    return windows, list(allclasses)
+
+
+
+
+def construct_xy(windows, classdict):
+    """
+    Input:
+        windows -- list of dataframes of all one class 
+    Output:
+        Xdata -- arrays of xyz data of each window stacked together
+        ydata -- integer class labels for each window
+    """
+    positions = ['accX', 'accY', 'accZ']
+    Xdata, ydata = [], []
+    for window in windows:
+        Xdata.append(window[positions].to_numpy())
+        ydata.append(classdict[window['behavior'].iloc[0]])
+        
+    return np.stack(Xdata), np.asarray(ydata)
