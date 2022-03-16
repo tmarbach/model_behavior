347bee2c8d7255ff47ac7f2fc5d40c392e18ad6d
Tyler <tymarbach@gmail.com>
2022-03-16 10:53:15 -0700

    added corr to transformations
    added the correlation  transformations to the xyz
    data. Order is xy, yz, xz. Still unsure about how
    to perform mean decrease in accuracy test.

--- bin/transformations.py
+++ bin/transformations.py
@@ -4,8 +4,13 @@ import pandas as pd
 
 def transform_xy(windows, classdict):
     """
+    Purpose:
+        Converts list of single class dataframes to two arrays
+        Xdata (all raw/transformed datapoints) & ydata (class label).
+        transformations included per axis :
+            mean, std, min, max, kurtosis, skew, corr (xy, yz, xz)
     Input:
-        windows -- list of dataframes of all one class 
+        windows -- list of dataframes of all one class
     Output:
         Xdata -- arrays of xyz data of each window stacked together
         ydata -- integer class labels for each window
@@ -20,6 +25,15 @@ def transform_xy(windows, classdict):
         alldata = np.append(alldata, [window[positions].max(axis = 0)], 0)
         alldata = np.append(alldata, [window[positions].kurtosis(axis = 0)], 0)
         alldata = np.append(alldata, [window[positions].skew(axis = 0)], 0)
+        alldata = np.append(
+            alldata,
+            [
+                window[positions[0]].corr(window[positions[1]]),
+                window[positions[1]].corr(window[positions[2]]),
+                window[positions[0]].corr(window[positions[2]])
+            ],
+            0
+            )
         Xdata.append(alldata)
 
         ydata.append(classdict[window['behavior'].iloc[0]])
